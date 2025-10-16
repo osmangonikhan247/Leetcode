303fb762fdc60a241815a6056658fa3812f0f141
Md. Osman Goni Khan <khan15-5175@diu.edu.bd>
2025-10-16 23:18:16 +0600

    First Commit

---
+++ 78.dart
@@ -0,0 +1,14 @@
+class Solution {
+  List<List<int>> subsets(List<int> nums) {
+    List<List<int>> result = [[]];
+
+    for (int num in nums) {
+      int size = result.length;
+      for (int i = 0; i < size; i++) {
+        result.add([...result[i], num]);
+      }
+    }
+
+    return result;
+  }
+}
