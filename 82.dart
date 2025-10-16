7aae61dad007966bdd17420e164b907904ec7a0b
Md. Osman Goni Khan <khan15-5175@diu.edu.bd>
2025-10-16 23:12:18 +0600

    First Commit

---
+++ 82.dart
@@ -0,0 +1,34 @@
+class ListNode {
+  int val;
+  ListNode? next;
+  ListNode([this.val = 0, this.next]);
+}
+
+class Solution {
+  ListNode? deleteDuplicates(ListNode? head) {
+    ListNode dummy = ListNode(0, head);
+    ListNode? prev = dummy;
+
+    while (head != null) {
+      bool isDuplicate = false;
+
+      // Check if next node has the same value
+      while (head!.next != null && head.val == head.next!.val) {
+        isDuplicate = true;
+        head = head.next;
+      }
+
+      if (isDuplicate) {
+        // Skip all nodes with duplicate value
+        prev!.next = head.next;
+      } else {
+        // Move prev only if current value is unique
+        prev = prev!.next;
+      }
+
+      head = head.next;
+    }
+
+    return dummy.next;
+  }
+}
